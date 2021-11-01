20e0ce467f31400ea61da6a5797045f854723b7c
drIsCoding <20715772+drIsCoding@users.noreply.github.com>
2021-11-01 19:21:22 -0400

    working stats as dictionary

---
+++ UserPrefs.Application/Users/Models/UsersInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserPrefs.Application.Users.Models
+{
+    public class UsersInfo
+    {
+        IEnumerable<User> Users { get; set; }
+        Dictionary<int, Dictionary<string, int>> ColorStats { get; set; }
+    }
+}
+
+
+//20: {
+//    Orange: 30,
+//    Red: 40
+//}
--- UserPrefs.Application/Users/Queries/GetUsers.cs
+++ UserPrefs.Application/Users/Queries/GetUsers.cs
@@ -17,7 +17,40 @@ namespace UserPrefs.Application.Users.Queries
         }
         public IEnumerable<User> GetAll()
         {
-            return _repository.GetAll();
+            var users = _repository.GetAll();
+            var stats = GenerateStats(users);
+            return users;
+        }
+
+        public void GetStats()
+        {
+            var users = GetAll();
+
+        }
+
+        private Dictionary<int, Dictionary<string, int>> GenerateStats(IEnumerable<User> users)
+        {
+            /*
+             returns:
+               <TopAgeRange<HexColor, NumPicked>>
+             */
+
+            var ceilings = new[] { 20, 30, 40, 50, 60, 70, 80 };
+
+            // combination of:
+            // https://stackoverflow.com/questions/1375997/group-by-variable-integer-range-using-linq
+            // https://stackoverflow.com/questions/9196796/group-a-collection-and-return-a-dictionary
+            // https://stackoverflow.com/questions/1918742/elegant-way-to-create-a-nested-dictionary-in-c-sharp
+            // https://stackoverflow.com/questions/7285714/linq-with-groupby-and-count
+
+            Dictionary<int, Dictionary<string, int>> userGroups = users.GroupBy(u => ceilings.First(ceiling => ceiling >= u.Age))
+                .ToDictionary(userGroup => userGroup.Key,
+                    userGroup => userGroup.GroupBy(users => users.ColorHex)
+                    .Select(colorGroup => new { color = colorGroup.Key, count = colorGroup.Count() })
+                    .ToDictionary(countedColors => countedColors.color, countedColors => countedColors.count));
+
+
+            return userGroups;
         }
     }
 }
