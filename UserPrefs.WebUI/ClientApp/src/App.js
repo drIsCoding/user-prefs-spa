7c8fc3472e022966b323b2483eaa1c890e23be61
drIsCoding <20715772+drIsCoding@users.noreply.github.com>
2021-11-02 17:18:34 -0400

    remove unused files, cleanup nav etc

--- UserPrefs.WebUI/ClientApp/src/App.js
+++ UserPrefs.WebUI/ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import UserPreferencesContainer from './components/userPreferences/userPreferencesContainer'
 
 import './custom.css'
@@ -17,11 +15,9 @@ export default class App extends Component {
     render() {
         return (
             <Layout>
-                <Route exact path='/' component={Home} />
-                <Route path='/counter' component={Counter} />
-                <Route path='/fetch-data' component={FetchData} />
-                <Route path='/user-preferences' component={UserPreferencesContainer} />
-                <Route path='/user-entry' component={UserEntryContainer} />
+                <Route exact path='/' component={Home} title="Home" />
+                <Route path='/user-preferences' component={UserPreferencesContainer} title="User Preferences"  />
+                <Route path='/user-entry' component={UserEntryContainer} title="User Entry"   />
             </Layout>
         );
     }
---
+++ UserPrefs.WebUI/ClientApp/src/components/Home.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export class Home extends Component {
+    static displayName = Home.name;
+
+    render() {
+        return (
+            <div>
+                <h1>Welcome!</h1>
+                <p>Are you a manager?</p>
+                <ul>
+                    <li><Link to="/user-preferences">View user preferences and add new users</Link></li>
+                </ul>
+                <p>Are you a user?</p>
+                <ul>
+                    <li><Link to="/user-entry">Add yourself to the system</Link></li>
+                </ul>
+            </div>
+        );
+    }
+}
